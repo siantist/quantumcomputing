bfd39486b8d974345686742f396d17b85662c98d
siantist <104287799+siantist@users.noreply.github.com>
2022-11-13 00:07:44 -0500

    Update amplitude_encoded_vectors.cpp

--- amplitude_encoded_vectors.cpp
+++ amplitude_encoded_vectors.cpp
@@ -10,3 +10,6 @@ var amp_enc_reg = qbit[num_qbits];
 amplitude_encode(vec, amp_enc_reg); 
 
 // note: check the right cpp function names
+
+// note: table 9-1 difference btwn amplitude and state encoding for vector data
+// ex 6-2 repeated amplitude amplification (flip and mirror is amplitude amplification) 
